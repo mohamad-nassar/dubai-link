4b41488f74c2323f99489d8cd521ab17389878d7
mohamad-nassar <mohamadnassar13.mn@gmail.com>
2023-01-24 07:41:38 +0200

    travel

--- src/components/Banner/BannerTourism.js
+++ src/components/Banner/BannerTourism.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import background from '../../assets/banner/tourismback.png';
+import url from '../url';
 function BannerTourism() {
+  const [image,setImage]=useState("");
+  async function getimage()
+  {
+    const response=await axios.get(`${url.baseURL}/travel`);
+    setImage(url.mediaURL+"/"+response.data.travel.image);
+  }
+  useEffect(()=>{
+    getimage();
+  },[])
   return (
     <div>
-    <div class="breadcrumb breadcrumb-style-one" style={{ backgroundImage: `url(${background})` }}>
+    <div class="breadcrumb breadcrumb-style-one" style={{ backgroundImage: `url(${image})` }}>
             <div class="container">
             <div class="col-lg-12 text-center d-flex justify-content-center">
             <h2 class="bottom-0 text-capitalize d-none d-md-block">Travel & Tourism</h2>
